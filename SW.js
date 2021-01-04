e3f50af33c79980180ef55f8757047e7849395cf
Humsbel Jao <humsbel_obregon12@hotmail.com>
2021-01-04 11:17:31 -0500

    Implemented offline storage

--- SW.js
+++ SW.js
@@ -1,6 +1,21 @@
+// Constants
+const CACHE_DATA = "offline-data";
+const STATIC_RESOURCES = ["index.html", "app.js", "logo.png"];
+
 // Install service worker
-self.addEventListener("install", async (e) => {
+self.addEventListener("install", async (e) => { //e = event
 	console.log("ServiceWorker install");
+
+	// Waita data
+	e.waitUntil(
+		(async () => {
+			const cache = await caches.open(CACHE_DATA);
+			return await cache.addAll(STATIC_RESOURCES);
+		})()
+	);
+
+	// skip waikinting data
+	self.skipWaiting();
 });
 
 // Listen for fetching request
